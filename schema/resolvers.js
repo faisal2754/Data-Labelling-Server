227b1a1b093eac12666bd92d8cd37ef765d57c87
Faisal <saleemfs27@gmail.com>
2021-08-25 17:18:16 +0200

    Set registration cookie

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -150,7 +150,11 @@ const resolvers = {
 
          user.jwt = jwt.sign({ user_id: user.user_id }, process.env.JWT_SECRET)
 
-         res.cookie('jwt', user.jwt, { httpOnly: true })
+         res.cookie('jwt', user.jwt, {
+            httpOnly: true,
+            sameSite: 'None',
+            secure: true
+         })
 
          return user
       },
