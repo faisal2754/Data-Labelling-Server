e9f70c49e139ad5c926e5c97ba74115c880c33d1
TristanBester <tristanbester@gmail.com>
2021-09-14 17:22:57 +0200

    Added headers.

--- server.js
+++ server.js
@@ -16,30 +16,34 @@ const startApolloServer = async () => {
    await server.start()
 
    const app = express()
-   // app.use(function (req, res, next) {
-   //    // Website you wish to allow to connect
-   //    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+   app.use(function (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader(
+         'Access-Control-Allow-Origin',
+         'https://faisal2754.github.io/Web-Data-Labelling-v2'
+      )
 
-   //    // Request methods you wish to allow
-   //    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      // Request methods you wish to allow
+      // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-   //    // Request headers you wish to allow
-   //    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      // Request headers you wish to allow
+      // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-   //    // Set to true if you need the website to include cookies in the requests sent
-   //    // to the API (e.g. in case you use sessions)
-   //    // res.setHeader('Access-Control-Allow-Credentials', true);
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      // res.setHeader('Access-Control-Allow-Credentials', true);
 
-   //    // Pass to next layer of middleware
-   //    next()
-   // })
+      // Pass to next layer of middleware
+      next()
+   })
    app.use(cookieParser())
    app.use(graphqlUploadExpress())
    app.use(
       cors({
          origin: [
             'http://localhost:3000',
-            'https://faisal2754.github.io/Web-Data-Labelling-v2'
+            'https://faisal2754.github.io/Web-Data-Labelling-v2',
+            'http://127.0.0.1:5500'
          ],
          credentials: true
       })
